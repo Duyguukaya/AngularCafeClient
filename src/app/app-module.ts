6c47c323cf8b4313a4f67c65a323efef27d77a84
Duyguukaya <Duyguukaya@users.noreply.github.com>
2026-02-12 21:40:28 +0300

    admin-layout oluşturuldu link denemesi yapıldı.

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -7,6 +7,9 @@ import { Home } from './ui-components/home/home';
 import { Contact } from './ui-components/contact/contact';
 import { UiLayout } from './layouts/ui-layout/ui-layout';
 import { AdminLayout } from './layouts/admin-layout/admin-layout';
+import { Category } from './admin-components/category/category';
+import { Menu } from './admin-components/menu/menu';
+import { About } from './admin-components/about/about';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { AdminLayout } from './layouts/admin-layout/admin-layout';
     Home,
     Contact,
     UiLayout,
-    AdminLayout
+    AdminLayout,
+    Category,
+    Menu,
+    About
   ],
   imports: [
     BrowserModule,
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -3,12 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { Home } from './ui-components/home/home';
 import { Contact } from './ui-components/contact/contact';
 import { UiLayout } from './layouts/ui-layout/ui-layout';
+import { AdminLayout } from './layouts/admin-layout/admin-layout';
+import { Category } from './admin-components/category/category';
+import { Menu } from './admin-components/menu/menu';
+import { About } from './admin-components/about/about';
 
 const routes: Routes = [
   //ui-Route
   {path:'',component:UiLayout,children:[
     {path:'',component:Home},
     {path:'contact',component:Contact}
+  ]},
+
+  //admin-Route
+  {path:'admin',component:AdminLayout,children:[
+    {path:'category',component:Category},
+    {path:'menu',component:Menu},
+    {path:'about',component:About}
   ]}
 
 ];
