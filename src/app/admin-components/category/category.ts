27491e800927f1d92161049d89250441171104ab
Duyguukaya <Duyguukaya@users.noreply.github.com>
2026-02-12 21:55:23 +0300

    .

--- src/app/admin-components/category/category.ts
+++ src/app/admin-components/category/category.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CategoryModel } from '../../models/categoryModel';
 
 @Component({
   selector: 'app-category',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './category.css',
 })
 export class Category {
+
+
+  categories:CategoryModel[] = [{id:1,name:'Kahveler'},{id:2,name:'Tatlılar'},{id:3,name:'İçecekler'}]
+
+
+  getCategories(){
+    return this.categories;
+  }
 
 }
