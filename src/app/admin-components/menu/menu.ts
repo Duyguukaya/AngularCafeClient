42668efadb6ad41a2fb47d5a22afbf03954d09fc
Duyguukaya <Duyguukaya@users.noreply.github.com>
2026-02-17 21:54:18 +0300

    menü crud methodları yazılsı, silme işlemi yapıldı. SweetAlert paketi kuruldu.

--- src/app/admin-components/menu/menu.ts
+++ src/app/admin-components/menu/menu.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MenuService } from '../../services/menu-service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-menu',
@@ -7,5 +10,41 @@ import { Component } from '@angular/core';
   styleUrl: './menu.css',
 })
 export class Menu {
+
+  private menuService = inject(MenuService);
+
+  menus= toSignal(this.menuService.getAll(),{
+    initialValue:[]
+  })
+
+  delete(id:number){
+    Swal.fire({
+  title: "Silmek istediğinize emin misiniz?",
+  text: "Bu işelemi geri alamazsınız!",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#3085d6",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Evet, Sil!",
+  cancelButtonText:"İptal"
+}).then((result) => {
+  if (result.isConfirmed) {
+
+    this.menuService.delete(id).subscribe({
+      complete:() =>{
+          Swal.fire({
+      title: "Silindi!",
+      text: "Silme işleminiz başarıyla gerçekleşti.",
+      icon: "success"
+    });
+
+    window.location.reload();
+      }
+    })
+
+
+  }
+});
+  }
 
 }
